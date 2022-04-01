644695e483008b02a2c670c67b83e8fa769581e9
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-01 10:22:10 -0700

    debug broken test and update code

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
 // UTILITY LOGIC 
 function noInputtedWord(input) {
-  if ((input.trim().length === 0)) {
+  let sInput = input.toString();
+  if ((sInput.trim().length === 0)) {
     return alert("Please enter a Number");
   }
 }
 
-function filterPunctuation(input) {
-  return input.split("").filter(function(number) {
-    return !".?,!;:'\"%$#@!^&*()_-+=\\|[]{}`~/".includes(number);
-  }).join("");
-}
+// function filterPunctuation(input) {
+//   return input.split("").filter(function(number) {
+//     return !".?,!;:'\"%$#@!^&*()_-+=\\|[]{}`~/".includes(number);
+//   }).join("");
+// }
 
 // BUSINESS LOGIC 
 function beepBoop(input) {
+  if (noInputtedWord(input) === true) {
+    return 0;
+  }
   let output = []
   for (let i = 0; i <= input; i++) {
     let strInput = i.toString();
     if (strInput.includes("3")) {
       output.push("Won't you be my neighbor?");
+    } else if (strInput.includes("2")) {
+      output.push("Boop");
     } else {
       output.push(i);
     } 
